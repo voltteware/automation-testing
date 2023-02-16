62fa8b0d27834e949da844e7c02c0ff4d78c3ebc
nancy.p <nancy.p@itcgroup.io>
2023-02-16 19:54:16 +0700

    Refactor code delete user api

--- features/step_definitions/api/Administrator/delete-user.steps.ts
+++ features/step_definitions/api/Administrator/delete-user.steps.ts
@@ -1,4 +1,4 @@
-import { When, Then, Given } from '@cucumber/cucumber';
+import { When, Then, Given, DataTable } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 import * as adminRequest from '../../../../src/api/request/administration.service';
 import * as registerRequest from '../../../../src/api/request/register.service';
@@ -33,7 +33,7 @@ Then(`{} send request GET to get users keys`, async function (actor: string) {
 Then('Check email exist in the system, if it does not exist will create user with email <testauto@gmail.com>', async function () {
     allUser = arrayHelper.flattenArray(getUsersResponseBody, 'data');
     const foundUser = allUser.find((element: { userId: any; }) => element.userId === `testauto@gmail.com`);
-    if(typeof foundUser == 'undefined'){
+    if (typeof foundUser == 'undefined') {
         this.payload = {
             firstName: 'Test',
             lastName: 'Auto',
@@ -49,7 +49,7 @@ Then('Check email exist in the system, if it does not exist will create user wit
         this.payloadLogin = {
             username: 'may27pre@gmail.com',
             password: 'Test1111!',
-          }
+        }
         this.loginResponse = await authenticateRequest.sendPOSTAuthenticatieRequest(this.request, Links.API_LOGIN, this.payloadLogin);
         if (this.loginResponse.status() == 201) {
             const responseHeaders = this.loginResponse.headers();
@@ -67,18 +67,74 @@ Then('Check email exist in the system, if it does not exist will create user wit
     }
 })
 
-Then('{} filters user to get user which he has the email', async function (actor: string) {
-    selectedUser = await getUsersResponseBody.find((us: any) => us.userId == `testauto@gmail.com`)
+Then('Check email exist in the system, if it does not exist will create user with below email', async function (dataTable: DataTable) {
+    var email: string = dataTable.hashes()[0].email
+
+    allUser = arrayHelper.flattenArray(getUsersResponseBody, 'data');
+    const foundUser = allUser.find((element: { userId: any; }) => element.userId === email);
+    if (typeof foundUser == 'undefined') {
+        this.payload = {
+            firstName: 'Test',
+            lastName: 'Auto',
+            companyName: 'ITC-Company-Testing',
+            companyType: 'ASC',
+            phone: '0355025511',
+            email: email,
+            password: 'Test1111!',
+        }
+        this.registerResponse = await registerRequest.sendPOSTRegisterRequest(this.request, Links.API_REGISTER, this.payload);
+        this.registerResponseBody = JSON.parse(await this.registerResponse.text())
+        // Login Admin after check
+        this.payloadLogin = {
+            username: 'may27pre@gmail.com',
+            password: 'Test1111!',
+        }
+        this.loginResponse = await authenticateRequest.sendPOSTAuthenticatieRequest(this.request, Links.API_LOGIN, this.payloadLogin);
+        if (this.loginResponse.status() == 201) {
+            const responseHeaders = this.loginResponse.headers();
+            this.cookieLogin = responseHeaders['set-cookie'];
+            this.loginResponseBody = JSON.parse(await this.loginResponse.text())
+        }
+        //Get list users after register 
+        const options = {
+            headers: this.cookieLogin
+        }
+        getUsersResponse = await adminRequest.getUser(this.request, Links.API_ADMIN_GET_USER, options);
+        if (getUsersResponse.status() == 200) {
+            getUsersResponseBody = JSON.parse(await getUsersResponse.text());
+        }
+    }
+})
+
+// Then('{} filters user to get user which he has the email', async function (actor: string) {
+//     selectedUser = await getUsersResponseBody.find((us: any) => us.userId == `testauto@gmail.com`)
+//     logger.log('info', `Response Body before filter: ${JSON.stringify(selectedUser, undefined, 4)}`);
+//     this.attach(`Response Body before filter: ${JSON.stringify(selectedUser, undefined, 4)}`);
+// })
+
+Then('{} filters user to get user which has the email as {}', async function (actor, expectedEmail: string) {
+    selectedUser = await getUsersResponseBody.find((us: any) => us.userId == expectedEmail)
     logger.log('info', `Response Body before filter: ${JSON.stringify(selectedUser, undefined, 4)}`);
     this.attach(`Response Body before filter: ${JSON.stringify(selectedUser, undefined, 4)}`);
 })
 
-Then('{} sends a DELETE method to delete user', async function (actor: string) {
+// Then('{} sends a DELETE method to delete user', async function (actor: string) {
+//     const options = {
+//         headers: this.headers
+//     }
+//     email = selectedUser.userId;
+//     this.response = await adminRequest.deleteUser(this.request, Links.API_ADMIN_DELETE_USER, email, options);
+//     deleteUsersResponseBody = JSON.parse(await this.response.text());
+// })
+
+Then('{} sends a DELETE method to delete user {}', async function (actor, expectedEmail: string) {
     const options = {
-      headers: this.headers
+        headers: this.headers
     }
-    email = selectedUser.userId;
-    this.response = await adminRequest.deleteUser(this.request, Links.API_ADMIN_DELETE_USER, email, options);
+    // Check if user does not provide expectedEmail => Use email get from selectedUser.userId
+    var emailWantToDelete = expectedEmail != '' ? expectedEmail : selectedUser.userId;
+
+    this.response = await adminRequest.deleteUser(this.request, Links.API_ADMIN_DELETE_USER, emailWantToDelete, options);
     deleteUsersResponseBody = JSON.parse(await this.response.text());
 })
 
