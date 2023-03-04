0a01886094880d4771b11406b01e6481ccdb0f39
Roni Nguyen <roni.n@itcgroup.io>
2023-03-04 15:15:03 +0700

    Create Supply

---
+++ src/utils/supplyPayLoad.ts
@@ -0,0 +1,14 @@
+export class payLoadSupply{
+    supplyUuid?: string
+    refNum?: string
+    vendorName?: string
+    vendorKey?: string
+    docDate?: Date
+    dueDate?: Date
+    itemName?: string
+    itemKey?: string
+    orderQty?: Number
+    openQty?: Number
+    orderKey?: string
+    rowKey?: string
+}
