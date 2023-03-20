fe20e34f2014cf854c99a9fa2078d7f8efb5e4f6
Priam Nguyen <priam.n@itcgroup.io>
2023-03-20 11:15:46 +0700

    fix delete user

--- src/helpers/array-helper.ts
+++ src/helpers/array-helper.ts
@@ -3,7 +3,7 @@ function flattenArray(nestedArray: Array<any>, nestedPropName: string): Array<an
 
     nestedArray.forEach(item => {
         result.push(item);
-        // Kiem tra xem co item con hay khong
+        // Check if the item is still available
         if (Array.isArray(item[nestedPropName])) {
             result = result.concat(flattenArray(item[nestedPropName], nestedPropName));
         }
