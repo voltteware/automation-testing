9eb6d49c2d126f29c545bd3aac569ee865f253d4
Thiago <thiago.t@itcgroup.xyz>
2023-04-10 13:27:05 +0700

    1844 - Fixed auto build on a branch

--- cucumber-report.js
+++ cucumber-report.js
@@ -32,7 +32,9 @@ var options = {
 
 reporter.generate(options);
 
-
+function sleep(s){
+  return new Promise(resolve => setTimeout(resolve, s * 1000));
+};
 const {uploadS3} = require('./s3-upload');
 async function uploadReportHtml() {
     await sleep(10);
