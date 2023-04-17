219b48daedf4b46ae7f0434a05b4598ec45e07d9
BeeNguyen <bee.n@itcgroup.io>
2023-04-17 13:57:26 +0700

    Ticket 1762 - Add new user to company

--- src/api/request/user.service.ts
+++ src/api/request/user.service.ts
@@ -18,7 +18,19 @@ async function changePassword(request: APIRequestContext, linkApi: string, payLo
     return updateResponse;
 }
 
+// Add to company
+async function addToCompany(request: APIRequestContext, linkApi: string, payLoad: any, header?: any) {
+    const url = `${linkApi}`;
+    logger.log('info', `Send POST request ${url} with ${JSON.stringify(payLoad, undefined, 4)}`);
+    const response = await request.post(url, {
+        data: payLoad,
+        headers: header
+    });
+    return response;
+}
+
 export {
     getUserInformation,
-    changePassword
+    changePassword,
+    addToCompany
 }
